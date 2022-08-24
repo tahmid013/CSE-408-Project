829f131405580399706bf64a6169b83b0e161800
tahmid013 <37360863+tahmid013@users.noreply.github.com>
2022-08-24 20:30:07 +0600

    Quiz Options upload UI fixed and change in practice.js

--- react-frontend/src/components/pages/Question/QuestionInputPage_loop.js
+++ react-frontend/src/components/pages/Question/QuestionInputPage_loop.js
@@ -13,7 +13,7 @@ import { AddOptions, AddQuestion, AddQuiz, AddQuizQuestion, getCategories, getOp
 import { useGlobalContext } from '../../../context';
 
 
-export default function QuastionInputPageLoop({nav_path}) {
+export default function QuastionInputPageLoop({ nav_path }) {
   const [ques_type, setQues_type] = useState('');
   const [category, setCategory] = useState('');
   const [question, setQuestion] = useState('');
@@ -37,29 +37,51 @@ export default function QuastionInputPageLoop({nav_path}) {
 
   const [options, setopts] = useState('');
 
-  const checkOptionMatched = ()  => {
+  const checkOptionMatched = () => {
 
-      if(answer == op_1 || answer == op_2 || answer == op_3 || answer == op_4 ){
-          setMatched(true);
-          handleSubmit();
-      }
+    if(is_op1_ans == true){      
+      console.log("Setting op1 as answer...." + answer);
+    }
+    else if(is_op2_ans == true){
+      console.log("Setting op2 as answer...." + answer);
+    }
+    else if(is_op3_ans == true){
 
-      else{
-          setMatched(false);
-      }
+      console.log("Setting op3 as answer...." + answer);
+    }
+    else if(is_op4_ans == true){
+      console.log("Setting op4 as answer...." + answer);
+    }
+    else{
+
+
+      console.log("Setting answer to null...." + answer);
+    }
+
+    if (answer != null) {
+      setMatched(true);
+      handleSubmit();
+
+    }
+
+    else {
+      setMatched(false);
+    }
 
   }
 
   const handleSubmit = async e => {
 
-   
+    console.log("Options: " +op_1 + " " + op_2 + " " + op_3 + " " + op_4);
+    console.log("Answer: " + answer);
+
     const op_added = await AddOptions(
-      op_1,op_2,op_3,op_4
+      op_1, op_2, op_3, op_4
     );
 
     console.log(op_added);
-        
-    console.log("Now opt id: "+op_added.id);
+
+    console.log("Now opt id: " + op_added.id);
     // to be fetched option id first
     setImage(null);
     console.log("Adding question");
@@ -77,10 +99,10 @@ export default function QuastionInputPageLoop({nav_path}) {
       localStorage.setItem('ques_set_id', uploaded.id);
       console.log(nav_path);
 
-      console.log(localStorage.getItem('clubId'));      
+      console.log(localStorage.getItem('clubId'));
       const quiz_added = await AddQuizQuestion(
-        localStorage.getItem('quiz-info'),uploaded.id
-        
+        localStorage.getItem('quiz-info'), uploaded.id
+
       );
 
 
@@ -94,7 +116,72 @@ export default function QuastionInputPageLoop({nav_path}) {
 
 
   const [answer_, setAnswer_] = useState(false);
-  const toggleAnswer = () => setAnswer_(!answer_);
+  const [is_op1_ans, setOp1_ans] = useState(false);
+  const [is_op2_ans, setOp2_ans] = useState(false);
+  const [is_op3_ans, setOp3_ans] = useState(false);
+  const [is_op4_ans, setOp4_ans] = useState(false);
+
+  const toggleAnswer = () => {
+      console.log("Toggling answer " + answer_ );
+      if (answer_ === false) {
+         
+         setAnswer_(true);
+      }
+      else {
+          setAnswer_(false)
+      }
+    }
+  const toggleOp1 = () => {
+    console.log("Toggling op1 " + is_op1_ans);
+    if (is_op1_ans === false) {
+      setAnswer(op_1);
+      setMatched(true);
+      setOp1_ans(true);
+    }
+    else {
+
+      setOp1_ans(false)
+    }
+  }
+
+  const toggleOp2 = () => {
+    console.log("Toggling op2 " + is_op2_ans);
+    if (is_op2_ans === false) {
+      setAnswer(op_2);
+      setMatched(true);
+      setOp2_ans(true);
+    }
+    else {
+
+      setOp2_ans(false)
+    }
+  }
+  const toggleOp3 = () => {
+    console.log("Toggling op3 " + is_op3_ans);
+    if (is_op3_ans === false) {
+      setAnswer(op_3);
+      setMatched(true);
+      setOp3_ans(true);
+    }
+    else {
+      setOp3_ans(false)
+    }
+  }
+  const toggleOp4 = () => {
+    console.log("Toggling op4 " + is_op4_ans);
+    if (is_op4_ans === false) {
+      setAnswer(op_4);
+      setMatched(true);
+      setOp4_ans(true);
+    }
+    else {
+      setOp4_ans(false)
+    }
+  }
+
+
+  
+
 
   const [ques_type_list, setques_type_list] = useState(['MCQ', 'Written', 'Fill In the Blanks']);
 
@@ -122,14 +209,14 @@ export default function QuastionInputPageLoop({nav_path}) {
   const [value, setValue] = useState(getInitialState);
 
   const handleChangeBtn = (e) => {
-    
+
     setValue(e.target.value);
     setCategory(value);
     setQues_type(value);
     console.log(value);
-    
+
   };
-  
+
 
 
   return (
@@ -137,27 +224,27 @@ export default function QuastionInputPageLoop({nav_path}) {
 
     <div className='clubinput'>
       <div>
-        
+
         <form >
-         
-        <div>
+
+          <div>
             <select value={value} onChange={handleChangeBtn}>
-                {ques_type_list && ques_type_list.map(l_s => {
-                     return <option value={`${l_s}`}>{l_s}</option>
-                })   
-                }
+              {ques_type_list && ques_type_list.map(l_s => {
+                return <option value={`${l_s}`}>{l_s}</option>
+              })
+              }
             </select>
-            
+
           </div>
 
           <div>
             <select value={value} onChange={handleChangeBtn}>
-                {cat_s && cat_s.map(c_s => {
-                     return <option value={`${c_s.name}`}>{c_s.name}</option>
-                })   
-                }
+              {cat_s && cat_s.map(c_s => {
+                return <option value={`${c_s.name}`}>{c_s.name}</option>
+              })
+              }
             </select>
-            
+
           </div>
 
           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
@@ -166,11 +253,12 @@ export default function QuastionInputPageLoop({nav_path}) {
             />
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          {/*<Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
             <TextField id="input-with-sx" label="Answer" variant="standard"
               onChange={e => setAnswer(e.target.value)}
             />
           </Box>
+            */}
 
           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
             <TextField id="input-with-sx" label="Point" variant="standard"
@@ -179,31 +267,36 @@ export default function QuastionInputPageLoop({nav_path}) {
           </Box>
 
           <div>
-            <input type="radio" value={true} name="option" onClick={toggleAnswer} />
+            <input type="radio" value={answer_} name="option" onClick={toggleAnswer} />
             <span>Give Options</span>
 
           </div>
           {answer_ ?
             <form >
               <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <input type="checkbox" value={true} name="option_1" onClick={toggleOp1} />
                 <TextField id="input-with-sx" label="Option 1" variant="standard"
                   onChange={e => setOption_1(e.target.value)}
                 />
               </Box>
 
               <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <input type="checkbox" value={true} name="option_2" onClick={toggleOp2} />
                 <TextField id="input-with-sx" label="Option 2" variant="standard"
                   onChange={e => setOption_2(e.target.value)}
                 />
               </Box>
 
               <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <input type="checkbox" value={true} name="option_3" onClick={toggleOp3} />
                 <TextField id="input-with-sx" label="Option 3" variant="standard"
+
                   onChange={e => setOption_3(e.target.value)}
                 />
               </Box>
 
               <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <input type="checkbox" value={true} name="option_4" onClick={toggleOp4} />
                 <TextField id="input-with-sx" label="Option 4" variant="standard"
                   onChange={e => setOption_4(e.target.value)}
                 />
@@ -212,10 +305,10 @@ export default function QuastionInputPageLoop({nav_path}) {
             </form>
 
 
-            : <div>You said no! </div>
+            : <div></div>
           }
 
-          { matched ? 
+          {matched ?
             <div></div>
             :
             <>
