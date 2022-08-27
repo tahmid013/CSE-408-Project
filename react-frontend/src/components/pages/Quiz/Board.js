417bfe464298ab3ff310bc655306890b47830975
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2022-08-28 04:07:02 +0600

    leadeboard improved

--- react-frontend/src/components/pages/Quiz/Board.js
+++ react-frontend/src/components/pages/Quiz/Board.js
@@ -1,53 +1,144 @@
-import React, { useState } from 'react'
-import Profiles from './Profiles';
-import { Leaderboard } from './Database';
-import { Button } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import Profiles from "./Profiles";
+import { Leaderboard } from "./Database";
+import { Button } from "@mui/material";
+import { getQuizTakenData } from "../../../services/user-services";
+import { getUser } from "../../../services/club-services";
 
 export default function Board() {
+  const [period, setPeriod] = useState(0);
+  const [leaderboardData, setLeaderboardData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [userList, setUserList] = useState([]);
 
-    const [period, setPeriod] = useState(0);
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
-  const handleClick = (e) => {
-     
-    setPeriod(e.target.dataset.id)
-  }
+
+  useEffect(() => {
+
+    setLoading(true);
+    const getData = async () => {
+      await getQuizTakenData().then((data) => {
+        setLeaderboardData(data);
+        
+      });
+      await sleep(500);
+    };
+
+    getData();
+   
+
+    //console.log(leaderboardData);
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const userList2 = await Promise.all(
+        leaderboardData&& leaderboardData.map(async (member) => {
+          const user = await getUser(member.user);
+          //console.log(user);
+          return {
+            id: user.id,
+            name: user.username,
+          };
+        })
+      );
+      setUserList(userList2);
+      console.log(userList);
+    };
+    getData().catch(console.error);
+  }, [leaderboardData]);
+
+  useEffect(() => {
+    console.log(leaderboardData);
+    setLoading(false);
+  }, [userList,]);
+
+  const handleClick =async (e) => {
+    console.log(userList);
+    console.log(leaderboardData);
+    setPeriod(e.target.dataset.id);
+  };
 
   return (
-    <div className="board">
-        <h1 className='leaderboard'>Leaderboard</h1>
+    <>
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <>
+          {leaderboardData ? (
+            <>
+              <div className="board">
+                <h1 className="leaderboard">Leaderboard</h1>  
 
-        <div className="duration">
-            <Button variant='contained' onClick={handleClick} data-id='7'>7 Days</Button>
-            <Button variant='contained' onClick={handleClick} data-id='30'>30 Days</Button>
-            <Button variant='contained' onClick={handleClick} data-id='0'>All-Time</Button>
-        </div>
+                <div className="duration">
+                  <Button variant="contained" onClick={handleClick} data-id="7">
+                    7 Days
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={handleClick}
+                    data-id="30"
+                  >
+                    30 Days
+                  </Button>
+                  <Button variant="contained" onClick={handleClick} data-id="0">
+                    All-Time
+                  </Button>
+                </div>
 
-        <Profiles Leaderboard={between(Leaderboard, period)}></Profiles>
+                {leaderboardData &&
+            (leaderboardData.length > 0) &&
+           
+            leaderboardData.map((value, index) => (
+              <div className="flex" key={index}>
+                <div className="item">
+                  {/* <img className="pr" src={value.img} alt="" /> */}
 
-    </div>
-  )
+                  <div className="info">
+                    <h3 className="name text-dark">{userList &&  (userList.length > 0)  && userList[index].name}</h3>
+                    {/* <span>{value.location}</span> */}
+                  </div>
+                </div>
+                <div className="item">
+                  <span>{userList &&  userList[0] && userList[0].username}</span>
+                  <span>{leaderboardData[index].score}</span>
+                </div>
+              </div>
+            ))}
+                {/* <Profiles
+                  Leaderboard={between(leaderboardData, period)}
+                ></Profiles> */}
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </>
+  );
 }
 
+function between(data, between) {
+  const today = new Date();
+  const previous = new Date(today);
+  previous.setDate(previous.getDate() - (between + 1));
 
+  let filter = data.filter((val) => {
+    let userDate = new Date(val.dt);
+    if (between == 0) return val;
+    return previous <= userDate && today >= userDate;
+  });
 
-function between(data, between){
-    const today = new Date();
-    const previous = new Date(today);
-    previous.setDate(previous.getDate() - (between + 1));
-
-    let filter = data.filter(val => {
-        let userDate = new Date(val.dt);
-        if (between == 0) return val;
-        return previous <= userDate && today >= userDate;
-    })
-
-    // sort with asending order
-    return filter.sort((a, b) => {
-        if ( a.score === b.score){
-            return b.score - a.score;
-        } else{
-            return b.score - a.score;
-        }
-    })
-
+  // sort with asending order
+  return filter.sort((a, b) => {
+    if (a.score === b.score) {
+      return b.score - a.score;
+    } else {
+      return b.score - a.score;
+    }
+  });
 }
